9a48a30a8bf4658fd10c0e1d599fb67665f2ed23
PiloucK <clkuznie@student.42.fr>
2021-05-15 17:59:34 +0200

    .

--- D04/ex00/CppClassPrep.sh
+++ D04/ex00/CppClassPrep.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-eval `resize`
+DIALOG_H="30"
+DIALOG_W="80"
+MENU_LIST_H="15"
 
-CHOICE_SAVING_FILE=".cppScript.choices"
+CHOICES_F=".cppScript.choices"
+LIST_TOCREATE_F=".cppScript.tocreate"
+LIST_CREATED_F=".cppScript.created"
+BACKUP_D=".cppScript.backup"
 
-echo $COLUMNS $LINES
+clean_exit() {
+    rm -rf $CHOICES_F $LIST_CREATED_F $LIST_TOCREATE_F
+    exit
+}
 
-# "Full init" "Complete project creation (folder + makefile + class init)" \ __TODO
 main_menu () {
     whiptail --title "Custom cpp file creation (big shout-out to 42)" \
         --menu "\nWould you like to\n" \
-        $(( 30 )) $(( 80 )) $(( 15 )) \
+        $DIALOG_H $DIALOG_W $MENU_LIST_H \
     "Class -->" "Create both .cpp and .hpp files for a brand new canonical class." \
     "Cpp   -->" "Create only a .cpp file." \
     "Hpp   -->" "Create only a .hpp file." \
@@ -18,14 +25,49 @@ main_menu () {
     "Exit  ..." "Exit from the script." 2>.cppScript.choices
 }
 
+cpp_create() {
+    whiptail --inputbox "Name the file" $DIALOG_H $DIALOG_W "example.cpp" 2>.cppScript.tocreate
+
+    if [[ "$?" == "0" ]]; then
+        FILE_NAME=$(cat .cppScript.tocreate).cpp
+
+        if ls | grep -qx "$FILE_NAME"; then
+            whiptail --yesno "The file named $FILE_NAME currently exists and would get overwritten\
+                \n\nAre you sure?" $DIALOG_H $DIALOG_W $MENU_LIST_H
+            if [ $? ]; then
+                clean_exit
+            fi
+        else
+            whiptail --yesno "Do you confirm the file name $FILE_NAME?" \
+                $DIALOG_H $DIALOG_W $MENU_LIST_H
+            if [ $? == 1 ]; then
+                echo toto
+                exit
+            else
+                exit
+            fi
+        fi
+    fi
+}
+
 main() {
 
     while true
     do
         main_menu
-        exit
+
+        if grep -q "Exit" .cppScript.choices; then
+            clean_exit
+        elif grep -q "Cpp" .cppScript.choices; then
+            cpp_create || continue
+        # elif grep -q "Back" .cppScript.choices; then
+        #     main_menu
+        else
+            clean_exit
+        fi
     done
 
+    exit
 }
 
 main
