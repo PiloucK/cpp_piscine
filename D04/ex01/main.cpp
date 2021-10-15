9b595393535bbfee651f8a52b30a0d745c8607eb
Clement KUZNIEWYCZ <clkuznie@e1r6p20.42.fr>
2021-10-15 18:33:23 +0200

    .

--- D04/ex01/main.cpp
+++ D04/ex01/main.cpp
@@ -9,14 +9,28 @@ int main()
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     i->makeSound(); //will output the cat sound!
     j->makeSound();
     meta->makeSound();
-    const WrongAnimal *k = new WrongCat();
-    k->makeSound();
+	std::cout << '\n';
 
+    const WrongAnimal *k = new WrongCat();
+	std::cout << *k;
+    k->makeSound();
+	std::cout << '\n';
+
+	Animal* toto = new Dog();
+	std::cout << *toto;
+	toto->makeSound();
+	*toto = *i;
+	std::cout << *toto;
+	toto->makeSound();
+	delete toto;
+
+	std::cout << '\n';
     delete meta;
     delete j;
     delete i;
