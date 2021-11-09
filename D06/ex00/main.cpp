c05a0e2b90afbf36f4182f28c5c45a72c7861241
Clement KUZNIEWYCZ <clkuznie@e1r5p13.42.fr>
2021-11-09 17:00:49 +0100

    .

--- D06/ex00/main.cpp
+++ D06/ex00/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Clkuznie <clkuznie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/05 16:34:52 by Clkuznie          #+#    #+#             */
-/*   Updated: 2021/11/05 16:39:10 by Clkuznie         ###   ########.fr       */
+/*   Created: 2021/11/08 14:53:20 by Clkuznie          #+#    #+#             */
+/*   Updated: 2021/11/09 16:22:39 by Clkuznie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 int
 main(
-    int ac
-    , char **av )
+	int ac
+	, char **av )
 {
-    return (0);
+	if (ac == 2) {
+		std::string number(av[1]);
+		double		numberAsDouble;
+
+		try
+		{
+			numberAsDouble = number.stod();
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		
+	}
+
+	return (EXIT_SUCCESS);
 }
--- D06/ex00/main.hpp
+++ D06/ex00/main.hpp
@@ -5,14 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Clkuznie <clkuznie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/05 16:34:56 by Clkuznie          #+#    #+#             */
-/*   Updated: 2021/11/05 16:48:29 by Clkuznie         ###   ########.fr       */
+/*   Created: 2021/11/04 14:01:37 by Clkuznie          #+#    #+#             */
+/*   Updated: 2021/11/09 16:18:03 by Clkuznie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_HPP
 # define MAIN_HPP
 
-# include <climits>
+# include <iostream>
+# include <sstream>
+# include <cstdlib>
+
+# ifdef DEBUG
+# 	define ASSERT(T) if (!(T)) { std::cerr << "\033[1;31mASSERT FAILED\033[0m " #T << std::endl; exit(1); } else { std::cerr << "\033[1;32mSUCCESS\033[0m " #T << '\n'; }
+# else
+# 	define ASSERT(T)
+# endif
+
+# define CLEAR_STDOUT std::cout << "\033[2J";
+# define WAIT_INPUT std::cin.get();
+# define START_INFO std::cout << "To start the tests or go to the next one, press ENTER\n";
+# define NEXT_TEST std::cin.get(); std::cout << "\033[2J"; std::cout << "To start the tests or go to the next one, press ENTER\n\n\n";
 
 #endif
