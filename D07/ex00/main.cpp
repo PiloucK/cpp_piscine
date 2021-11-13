c2b8e20fc3d3dbcbee7df05b5441711aaeca0b17
Clement KUZNIEWYCZ <clkuznie@e1r6p3.42.fr>
2021-11-13 12:13:23 +0100

    .

--- D07/ex00/main.cpp
+++ D07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Clkuznie <clkuznie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 14:53:20 by Clkuznie          #+#    #+#             */
-/*   Updated: 2021/11/11 17:51:07 by Clkuznie         ###   ########.fr       */
+/*   Updated: 2021/11/13 12:11:24 by Clkuznie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,25 @@ int
 main(
 	void )
 {
+	{
+		int a = 2;
+		int b = 3;
+		
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+
+	NEXT_TEST
 	{
 		int i = 0, j = 4;
 
